9963bceec28a0256cd4bc934e61b31b8bc3cb1c3
Tom O'Connor <tom@twinhelix.org>
2021-12-29 22:42:30 +0000

    Added some better logs and a few edge-case URLs.

--- main.go
+++ main.go
@@ -11,17 +11,24 @@ const RSS_URL = NEW_URL + "rss.xml"
 
 func redirect(w http.ResponseWriter, r *http.Request) {
 	request_url := strings.Split(r.URL.Path, "/")
-	folder := request_url[1]
-	log.Println("Folder: ", folder)
-	log.Println("Request: ", r.URL.Path)
-	if folder == "blogish" {
-		slug := request_url[2]
-		redirect_url := NEW_URL + slug
-		log.Println("Redirected to ", redirect_url)
-		http.Redirect(w, r, redirect_url, http.StatusMovedPermanently)
-	} else if folder == "rss" {
-		log.Println("Redirected to ", RSS_URL)
-		http.Redirect(w, r, RSS_URL, http.StatusMovedPermanently)
+	if r.URL.Path != "/" {
+		folder := request_url[1]
+		log.Println("Folder: ", folder)
+		log.Println("Request: ", r.URL.Path)
+		if request_url[2] == "epic-fail" {
+			log.Println("Redirected to epic fail")
+			http.Redirect(w, r, "https://blog.devopstom.com/raid-is-not-backup/", http.StatusMovedPermanently)
+		} else if folder == "blogish" {
+			slug := request_url[2]
+			redirect_url := NEW_URL + slug
+			log.Println("Redirected to ", redirect_url)
+			http.Redirect(w, r, redirect_url, http.StatusMovedPermanently)
+		} else if folder == "rss" {
+			log.Println("Redirected to ", RSS_URL)
+			http.Redirect(w, r, RSS_URL, http.StatusMovedPermanently)
+		} else {
+			http.Redirect(w, r, NEW_URL, http.StatusMovedPermanently)
+		}
 	} else {
 		http.Redirect(w, r, NEW_URL, http.StatusMovedPermanently)
 	}
